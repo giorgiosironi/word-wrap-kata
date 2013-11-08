1407633d66f11111e12bd93fba90a7ed90c9aa99
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-11-08 22:41:05 +0100

    Added an exploratory test for the customer feedback

--- WrapperTest.php
+++ WrapperTest.php
@@ -134,6 +134,11 @@ class WrapperTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    public function testExploratory()
+    {
+        echo PHP_EOL, Wrapper::wrap(" You write a class called Wrapper, that has a single static function named wrap that takes two arguments, a string, and a column number. The function returns the string, but with line breaks inserted at just the right places to make sure that no line is longer than the column number. You try to break lines at word boundaries. ", 20);
+    }
+
     private function assertMultipleLines(/*$line, $line, ..., $arrayOfLines*/)
     {
         $arguments = func_get_args();
