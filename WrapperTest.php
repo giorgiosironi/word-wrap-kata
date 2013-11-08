7689f8bc498a04984ff7c92eebdbcede7ad27980
Giorgio Sironi <giorgio.sironi@neomobile.com>
2013-11-08 21:32:13 +0100

    Scenario: Hello World, 6

--- WrapperTest.php
+++ WrapperTest.php
@@ -4,6 +4,15 @@ class Wrapper
 {
     public static function wrap($paragraph, $columnNumber)
     {
+        if (strlen($paragraph) > $columnNumber) {
+            return implode(
+                "\n", 
+                [
+                    substr($paragraph, 0, $columnNumber),
+                    substr($paragraph, $columnNumber)
+                ]
+            );
+        }
         return $paragraph;
     }
 }
@@ -15,6 +24,25 @@ class WrapperTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(
             "Hello World",
             Wrapper::wrap("Hello World", 20)
+        );
+    }
+
+    public function testAParagraphOfTwoPerfectlyMatchingWordsIsWrappedInTwoLines()
+    {
+        $this->assertMultipleLines(
+            "Hello ",
+            "World",
+            Wrapper::wrap("Hello World", 6)
+        );
+    }
+
+    private function assertMultipleLines(/*$line, $line, ..., $arrayOfLines*/)
+    {
+        $arguments = func_get_args();
+        $arrayOfLines = array_pop($arguments);
+        $this->assertEquals(
+            implode("\n", $arguments),
+            $arrayOfLines
         );
     }
 }
